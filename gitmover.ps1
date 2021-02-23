b2603d57dbc3f371ed4827e2a883b5d0d38bf250
Ali Şentürk <ali.senturk@gmail.com>
2021-02-23 09:31:12 +0300

    Add files via upload

---
+++ gitmover.ps1
@@ -0,0 +1,37 @@
+﻿Function GitFetchRemoteBranches{
+   
+    $orginal = (git symbolic-ref HEAD).split("/")[-1]
+       
+    Foreach($entry in (git branch -r)){
+        
+        If($entry -like "*->*"){
+            $branch = $entry.split("->")[2].split("/")[1]
+        }else{
+            $branch = $entry.split("/")[1]
+        }
+
+        git checkout -b $branch origin/$branch
+
+        Remove-Variable branch -Force
+    }
+}
+
+
+$oldrepo = Read-Host "Eski Repo Adresi "
+$newrepo = Read-Host "Yeni Repo Adresi "
+$appname = $oldrepo.split("/")[-1]
+
+git clone $oldrepo
+cd $appname
+
+git fetch origin
+
+GitFetchRemoteBranches
+
+git remote add new-origin $newrepo
+git push --all new-origin
+git remote -v
+git remote rm origin
+git remote rename new-origin origin
+
+cd ..
